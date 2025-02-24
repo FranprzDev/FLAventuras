9de2c51cddc96a502446f986f310b6e720afcae4
Francisco Perez <francisco.perez@rollingcodestudio.com>
2025-02-24 12:07:27 -0300

    change the person to mayorPerson

--- lib/transversal/Supabase/supabase.ts
+++ lib/transversal/Supabase/supabase.ts
@@ -3,6 +3,5 @@ const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ??
   "https://cxsdwkqjqhtdwmvrpvjy.supabase.co";
 const supabaseKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ??
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4c2R3a3FqcWh0ZHdtdnJwdmp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1ODk2MTIsImV4cCI6MjA1MTE2NTYxMn0.qNXxPHxFm0EgDgbdaSftn4rXOkMpSIF0tZRighV3sys";
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
 export const supabase = createClient(supabaseUrl, supabaseKey);
